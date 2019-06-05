2c0d8cab39090064119b3a76870414542378b32a
Nikita Zonov <nzonov@gmail.com>
2019-06-05 23:54:15 +0300

    Открываем порт через 5 сек Ретрай каждые 30 сек При получении команды, переключаем контекс для ответа. В это время новые команды не принемаются (выкидываются просто)

--- src/main/scala/actors/Terminal.scala
+++ src/main/scala/actors/Terminal.scala
@@ -1,47 +1,47 @@
 package actors
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated, actorRef2Scala}
+import scala.concurrent.duration._
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated, Timers, actorRef2Scala}
 import akka.io.IO
 import akka.serial.Serial
 import akka.util.ByteString
 import models.dreamkas.{DeviceSettings, Out}
+import services.HttpService
 import utils.helpers.ArrayByteHelper._
 
-class Terminal(deviceSettings: DeviceSettings) extends Actor with ActorLogging {
+class Terminal(deviceSettings: DeviceSettings) extends Actor with ActorLogging with Timers {
 
   import Terminal._
   import context._
 
-  val reader = actorOf(Props[ConsoleReader])
+  timers.startSingleTimer(StartTimer, Start, 2.seconds)
 
-  log.info(s"Requesting manager to open port: ${deviceSettings.port}, baud: ${deviceSettings.serialSettings.baud}")
-  IO(Serial) ! Serial.Open(deviceSettings.port, deviceSettings.serialSettings)
+  val reader: ActorRef = actorOf(Props[ConsoleReader])
 
   override def postStop(): Unit = {
     system.terminate()
   }
 
-  def receive = {
-    case Serial.CommandFailed(cmd, reason) => {
-      log.error(s"Connection failed, stopping terminal. Reason: ${reason.getLocalizedMessage}")
-      context stop self
-    }
-    case Serial.Opened(port) => {
+  def receive: Receive = {
+    case Start => timers.startPeriodicTimer(ReconnectTimer, OpeningSerial, 30.seconds)
+      self ! OpeningSerial
+    case OpeningSerial =>
+      log.info(s"Requesting manager to open port: ${deviceSettings.port}, baud: ${deviceSettings.serialSettings.baud}")
+      IO(Serial) ! Serial.Open(deviceSettings.port, deviceSettings.serialSettings)
+
+    case Serial.CommandFailed(cmd, reason) =>
+      log.error(s"Connection failed, retrying. Error: ${reason.getLocalizedMessage}")
+    case Serial.Opened(port) =>
       log.info(s"Port $port is now open.")
+      timers.cancel(ReconnectTimer)
       val operator = sender
       context become opened(operator)
       context watch operator
       reader ! ConsoleReader.Read
-    }
   }
 
   def opened(operator: ActorRef): Receive = {
-
-    case Serial.Received(data) => Out(data).dump
-      log.info(s"Received data: ${formatData(data)}")
-
-    case Terminal.Wrote(data) => log.info(s"Wrote data: ${formatData(data)}")
-
     case Serial.Closed => log.info("Operator closed normally, exiting terminal.")
       context unwatch operator
       context stop self
@@ -58,12 +58,32 @@ class Terminal(deviceSettings: DeviceSettings) extends Actor with ActorLogging {
 
     case ConsoleReader.Command(input) => val data = ByteString(input)
       operator ! Serial.Write(data, length => Wrote(data.take(length)))
+      context become waitingResponse(operator, sender)
+    case HttpService.OpenSession(name) =>
+  }
+
+  def waitingResponse(operator: ActorRef, sender: ActorRef): Receive = {
+
+    case Terminal.Wrote(data) => log.info(s"Wrote data: ${formatData(data)}")
+
+    case Serial.Received(data) => val out = Out(data)
+      log.info(s"Received data: ${formatData(data)}")
+      context become opened(operator)
+      sender ! out
       reader ! ConsoleReader.Read
   }
 
 }
 
 object Terminal {
+
+  private object ReconnectTimer
+
+  private object StartTimer
+
+  private case object Start
+
+  case object OpeningSerial
 
   case class Wrote(data: ByteString) extends Serial.Event
 
