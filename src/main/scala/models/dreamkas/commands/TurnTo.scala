6caa2b872bdf3817305294ae7788357f66193b76
Nikita Zonov <nzonov@gmail.com>
2019-06-16 21:09:36 +0300

    по умолчанию текущие дата и время

--- src/main/scala/models/dreamkas/commands/TurnTo.scala
+++ src/main/scala/models/dreamkas/commands/TurnTo.scala
@@ -7,7 +7,9 @@ import akka.util.ByteString
 import models.dreamkas.Password
 import utils.helpers.ListHelper.ListStringExtended
 
-final case class TurnTo(date: LocalDate, time: LocalTime)(implicit val password: Password) extends Command {
+final case class TurnTo(
+  date: LocalDate = LocalDate.now(), time: LocalTime = LocalTime.now()
+)(implicit val password: Password) extends Command {
 
   private val dataString = DateTimeFormatter.ofPattern("ddMMyy").format(date)
   private val timeString = DateTimeFormatter.ofPattern("HHmmss").format(time)
