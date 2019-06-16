8cf4e2a200529e6d9d2a7fb50d85823a8122c133
Nikita Zonov <nzonov@gmail.com>
2019-06-16 21:09:01 +0300

    стартуем с двумя принтерами если они указаны в конфигурации

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,5 @@
 import actors.Terminal
 import akka.actor.ActorSystem
-import akka.serial.Serial
 import services.{ConfigService, HttpService}
 import utils.Logging
 
@@ -8,13 +7,20 @@ object Main extends App with Logging {
 
   log.info("Starting terminal system, enter :q to exit.")
 
-  ConfigService.getPrinter("printer1").map { deviceSettings =>
-    log.info(s"deviceSettings: $deviceSettings")
-    Serial.debug(ConfigService.getSerialDebug)
-    val system = ActorSystem("akka-serial")
-    val terminal = system.actorOf(Terminal(deviceSettings), name = "terminal")
+  ConfigService.getPrinter("printer1").map { device1Settings =>
 
-    val http = new HttpService(terminal, None)
+    log.info(s"deviceSettings: $device1Settings")
+
+    val system = ActorSystem("akka-serial")
+    val printer1 = system.actorOf(Terminal(device1Settings), name = "printer1")
+
+    val printer2 = ConfigService.getPrinter("printer2")
+      .map { device2Settings =>
+        log.info(s"deviceSettings: $device2Settings")
+        system.actorOf(Terminal(device2Settings), name = "printer2")
+      }
+
+    val http = new HttpService(printer1, printer2)
 
     system.registerOnTermination {
       log.info("Stopped terminal system.")
